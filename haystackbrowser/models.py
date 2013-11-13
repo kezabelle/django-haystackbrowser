46fa79c818fec025b98176e254d6a641c33f8e57
Keryn Knight <keryn@kerynknight.com>
2013-11-13 19:36:29 +0000

    Probably fixed #3 A `content_type` field on the SearchIndex clobbers the `content_type` method on the SearchResult instance because of the underlying __dict__ being set directly.
    Thanks to Ben Hastings for reporting the issue.

--- haystackbrowser/models.py
+++ haystackbrowser/models.py
@@ -71,6 +71,17 @@ class SearchResultWrapper(object):
         except NoReverseMatch:
             return None
 
+    def get_detail_url(self):
+        try:
+            urlname = '%s:haystackbrowser_haystackresults_change' % self.admin
+            return reverse(urlname, kwargs={
+                'content_type': '.'.join(self.object.app_label,
+                                         self.object.model_name),
+                'pk': self.object.pk})
+        except NoReverseMatch:
+            return None
+
+
     def get_additional_fields(self):
         """Find all fields in the Haystack SearchResult which have not already
         appeared in the stored fields.
