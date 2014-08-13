8daacbf4281b29ed45da8bd56413a9566b5d3ee2
Keryn Knight <keryn@kerynknight.com>
2014-08-13 08:35:48 +0100

    Add in new model objects for faceting. Introduces AppliedFacet, which contains the data for one single facet's data. Introduces AppliedFacets, which just wraps over AppliedFacet Introduces Facet, which is a slightly richer object for a single facet field name. Modifies FacetWrapper to group by facet fields when returning data.

--- haystackbrowser/models.py
+++ haystackbrowser/models.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
+from copy import deepcopy
+from urllib import quote_plus
+from operator import itemgetter
+from itertools import groupby
+from collections import namedtuple
 from django.db import models
 try:
     from django.utils.encoding import force_text
@@ -8,6 +14,10 @@ from django.utils.safestring import mark_safe
 from django.utils.html import strip_tags
 from django.core.urlresolvers import NoReverseMatch, reverse
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+
+logger = logging.getLogger(__name__)
 
 
 class HaystackResults(models.Model):
@@ -193,13 +203,29 @@ class FacetWrapper(object):
     A simple wrapper around `sqs.facet_counts()` to filter out things with
     0, and re-arrange the data in such a way that the template can handle it.
     """
+    __slots__ = ('dates', 'fields', 'queries', '_total_count', '_querydict')
 
-    def __init__(self, facet_counts):
+    def __init__(self, facet_counts, querydict):
         self.dates = facet_counts.get('dates', {})
         self.fields = facet_counts.get('fields', {})
         self.queries = facet_counts.get('queries', {})
 
         self._total_count = len(self.dates) + len(self.fields) + len(self.queries)
+        # querydict comes from the cleaned form data ...
+        page_key = 'p'
+        if querydict is not None and page_key in querydict:
+            querydict.pop(page_key)
+        self._querydict = querydict
+
+    def __repr__(self):
+        return '<%(module)s.%(cls)s fields=%(fields)r dates=%(dates)r ' \
+               'queries=%(queries)r>' % {
+            'module': self.__class__.__module__,
+            'cls': self.__class__.__name__,
+            'fields': self.fields,
+            'dates': self.dates,
+            'queries': self.queries,
+        }
 
     def get_facets_from(self, x):
         if x not in ('dates', 'queries', 'fields'):
@@ -207,17 +233,27 @@ class FacetWrapper(object):
 
         for field, items in getattr(self, x).items():
             for content, count in items:
-                if count > 0:
-                    yield {'field': field, 'value': content, 'count': count}
+                content = content.strip()
+                if count > 0 and content:
+                    yield {'field': field, 'value': content, 'count': count,
+                           'fieldvalue': quote_plus('%s:%s' % (field, content)),
+                           'facet': Facet(field, querydict=self._querydict)}
+
+    def get_grouped_facets_from(self, x):
+        data = sorted(self.get_facets_from(x), key=itemgetter('field'))
+        #return data
+        results = ({'grouper': Facet(key), 'list': list(val)}
+                   for key, val in groupby(data, key=itemgetter('field')))
+        return results
 
     def get_field_facets(self):
-        return self.get_facets_from('fields')
+        return self.get_grouped_facets_from('fields')
 
     def get_date_facets(self):
-        return self.get_facets_from('dates')
+        return self.get_grouped_facets_from('dates')
 
     def get_query_facets(self):
-        return self.get_facets_from('queries')
+        return self.get_grouped_facets_from('queries')
 
     def __bool__(self):
         """
@@ -232,3 +268,112 @@ class FacetWrapper(object):
         For checking things via `if len(facets) > 0: print(facets)`
         """
         return self._total_count
+
+
+class AppliedFacet(namedtuple('AppliedFacet', 'field value querydict')):
+    __slots__ = ()
+    def title(self):
+        return self.value
+
+    @property
+    def facet(self):
+        """ a richer object """
+        return Facet(self.raw)
+
+    @property
+    def raw(self):
+        """ the original data, rejoined """
+        return '%s:%s' % (self.field, self.value)
+
+    @property
+    def narrow(self):
+        """ returns a string format value """
+        return '{0}:"{{cleaned_value}}"'.format(self.field)
+
+    def link(self):
+        """ link to just this facet """
+        new_qd = self.querydict.copy()
+        page_key = 'p'
+        if page_key in new_qd:
+            new_qd.pop(page_key)
+        new_qd['selected_facets'] = self.raw
+        new_qd['possible_facets'] = self.field
+        return '?%s' % new_qd.urlencode()
+
+    def remove_link(self):
+        new_qd = self.querydict.copy()
+        # remove page forcibly ...
+        page_key = 'p'
+        if page_key in new_qd:
+            new_qd.pop(page_key)
+        # remove self from the existing querydict/querystring ...
+        key = 'selected_facets'
+        if key in new_qd and self.raw in new_qd.getlist(key):
+            new_qd.getlist(key).remove(self.raw)
+        return '?%s' % new_qd.urlencode()
+
+
+class AppliedFacets(object):
+    __slots__ = ('_applied',)
+
+    def __init__(self, querydict):
+        self._applied = {}
+        selected = ()
+        if 'selected_facets' in querydict:
+            selected = querydict.getlist('selected_facets')
+        for raw_facet in selected:
+            if ":" not in raw_facet:
+                continue
+            field, value = raw_facet.split(":", 1)
+            to_add = AppliedFacet(field=field, value=value,
+                                  querydict=querydict)
+            self._applied[raw_facet] = to_add
+
+    def __iter__(self):
+        return iter(self._applied.values())
+
+    def __len__(self):
+        return len(self._applied)
+
+    def __contains__(self, item):
+        return item in self._applied
+
+    def __repr__(self):
+        raw = tuple(v.raw for k, v in self._applied.items())
+        return '<{cls!s}.{name!s} selected_facets={raw}>'.format(
+            cls=self.__class__.__module__, name=self.__class__.__name__,
+            raw=raw)
+
+    def __str__(self):
+        raw = [v.facet.get_display() for k, v in self._applied.items()]
+        return '{name!s} {raw!s}'.format(name=self.__class__.__name__, raw=raw)
+
+
+class Facet(object):
+    """
+    Takes a facet field name, like `thing_exact`
+    """
+
+    __slots__ = ('fieldname', '_querydict')
+    def __init__(self, fieldname, querydict=None):
+        self.fieldname = fieldname
+        self._querydict = querydict
+
+    def __repr__(self):
+        return '<%(module)s.%(cls)s - %(field)s>' % {
+            'module': self.__class__.__module__,
+            'cls': self.__class__.__name__,
+            'field': self.fieldname,
+        }
+
+    def link(self):
+        qd = self._querydict
+        if qd is not None:
+            return '?%s' % qd.urlencode()
+        return '?'
+
+    def get_display(self):
+        return self.fieldname.replace('_', ' ').title()
+
+    def choices(self):
+        return (self.fieldname, self.get_display())
